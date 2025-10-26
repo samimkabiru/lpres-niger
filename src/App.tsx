9905f69ddde9fc04b14c590f78eade4565dc820d
samimkabiru <kabirusamimadeiza@gmail.com>
2025-10-26 21:59:08 +0100

    Add a 'jump to top' button.

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import FrameworkSection from './components/FrameworkSection';
 import HeaderSection from './components/HeaderSection';
 import ValueChainsSection from './components/ValueChainsSection';
 import FooterSection from './components/FooterSection';
+import { FaArrowUp } from 'react-icons/fa';
 
 const App = () => {
   AOS.init({
@@ -14,6 +15,12 @@ const App = () => {
 
   return (
     <div>
+      <button
+        onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}
+        className="inline-flex w-8 h-8 justify-center items-center fixed bottom-5 right-5 z-20 cursor-pointer rounded-full text-white bg-green-900 hover:bg-green-800 transition-colors"
+      >
+        <FaArrowUp />
+      </button>
       <HeaderSection />
       <ValueChainsSection />
       <FrameworkSection />
