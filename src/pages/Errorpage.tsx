ead83df8731dd18c55c86bdf48a423acecf0b067
Samim Kabiru <kabirusamimadeiza@gmail.com>
2025-11-20 18:49:29 +0100

    Build the error page.

--- src/pages/Errorpage.tsx
+++ src/pages/Errorpage.tsx
@@ -1,5 +1,33 @@
+import { isRouteErrorResponse, Link, useRouteError } from 'react-router-dom';
+
 const Errorpage = () => {
-  return <div>Errorpage</div>;
+  const error = useRouteError();
+
+  return (
+    <div className="text-center px-4 py-24">
+      {isRouteErrorResponse(error) && (
+        <h1 className="font-bold text-green-950 text-4xl md:text-6xl lg:text-9xl">
+          404
+        </h1>
+      )}
+      <p className="uppercase mt-4 text-gray-700 font-bold lg:text-3xl">
+        {isRouteErrorResponse(error) && 'oops! page not found'}
+      </p>
+      <p className="text-2xl mt-2">
+        {isRouteErrorResponse(error)
+          ? "Sorry, the page you're looking for doesn't exist."
+          : 'Sorry, an unexpected error occurred.'}
+      </p>
+      {isRouteErrorResponse(error) && (
+        <Link
+          className="mt-3 inline-block hover:underline text-green-900"
+          to="/"
+        >
+          Back to Homepage
+        </Link>
+      )}
+    </div>
+  );
 };
 
 export default Errorpage;
