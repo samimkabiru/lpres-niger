52f1667bee13b09515b7acde6dcdd0183787f1ee
Samim Kabiru <kabirusamimadeiza@gmail.com>
2025-11-20 18:55:31 +0100

    Clean up react leaflet from the project.

--- src/pages/Errorpage.tsx
+++ src/pages/Errorpage.tsx
@@ -6,14 +6,12 @@ const Errorpage = () => {
   return (
     <div className="text-center px-4 py-24">
       {isRouteErrorResponse(error) && (
-        <h1 className="font-bold text-green-950 text-4xl md:text-6xl lg:text-9xl">
-          404
-        </h1>
+        <h1 className="font-bold text-green-950 text-6xl lg:text-9xl">404</h1>
       )}
-      <p className="uppercase mt-4 text-gray-700 font-bold lg:text-3xl">
+      <p className="uppercase mt-4 text-2xl text-gray-700 font-bold lg:text-3xl">
         {isRouteErrorResponse(error) && 'oops! page not found'}
       </p>
-      <p className="text-2xl mt-2">
+      <p className="sm:text-2xl mt-2">
         {isRouteErrorResponse(error)
           ? "Sorry, the page you're looking for doesn't exist."
           : 'Sorry, an unexpected error occurred.'}
